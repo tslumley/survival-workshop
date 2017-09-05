4873ff30a9d6555419f51fba3da79e79a76bb529
Thomas Lumley <t.lumley@auckland.ac.nz>
2017-09-05 13:05:20 +1200

    up to logrank test

--- kmsurv.R
+++ kmsurv.R
@@ -27,3 +27,19 @@ t1<-sapply(0:9, function(from) with(pbc, dies(time/365,status, from,from+1)))
 
 thalf<-sapply(seq(0,9.5,by=0.5), function(from) with(pbc, dies(time/365,status, from,from+0.5)))	
 
+tmonth<-sapply(seq(0,9+11/12,by=1/12), function(from) with(pbc, dies(time/365,status, from,from+1/12)))	
+
+## Individual points get noisier
+plot(1:10,t1,type="h",xlab='Years',ylab="Proportion dying",ylim=range(0,t1))
+plot(seq(1/12,10,by=1/12),tmonth,type="h",xlab='Years',ylab="Proportion dying")
+
+
+## survival function doesn't
+plot(c(0,10), c(1,cumprod(1-t10)),type="s",xlab="years",ylab="Proportion alive", ylim=c(0,1))
+lines(c(0,5, 10), c(1, cumprod(1-t5)),type="s",lty=2)
+lines(c(0,2,4,6,8,10), c(1, cumprod(1-t2)),type="s",lty=3)
+
+lines(c(0:10), c(1, cumprod(1-t1)),type="s",lty=1,col="blue")
+lines(seq(0,10,by=0.5), c(1, cumprod(1-thalf)),type="s",lty=3,col="blue")
+lines(seq(0,10,by=1/12), c(1, cumprod(1-tmonth)),type="s",lty=1,col="goldenrod")
+
--- redistribute.R
+++ redistribute.R
@@ -3,7 +3,7 @@ d<-subset(aml,x=="Maintained")
 p<-rep(1/11,11)
 P<-rev(cumsum(rev(p)))
 f<-with(d, stepfun(c(time),c(P,0)))
-plot(f, main="",xlab="time",ylab="Proportion",pch=1+18*d$status)
+plot(f, main="",xlab="time",ylab="Proportion",pch=1+3*d$status)
 
 p[4:11]<-p[4:11]+p[3]/length(4:11)
 p[3]<-0
@@ -11,7 +11,7 @@ P<-rev(cumsum(rev(p)))
 f<-with(d, stepfun(time,c(P,0)))
 lines(f,col="red")
 
-plot(f, main="",xlab="time",ylab="Proportion",pch=1+18*d$status)
+plot(f, main="",xlab="time",ylab="Proportion",pch=1+3*d$status)
 
 p[7:11]<-p[7:11]+p[6]/length(7:11)
 p[6]<-0
@@ -19,7 +19,7 @@ P<-rev(cumsum(rev(p)))
 f<-with(d, stepfun(time,c(P,0)))
 lines(f,col="red")
 
-plot(f, main="",xlab="time",ylab="Proportion",pch=1+18*d$status)
+plot(f, main="",xlab="time",ylab="Proportion",pch=1+3*d$status)
 
 p[10:11]<-p[10:11]+p[9]/length(10:11)
 p[9]<-0
@@ -27,7 +27,7 @@ P<-rev(cumsum(rev(p)))
 f<-with(d, stepfun(time,c(P,0)))
 lines(f,col="red")
 
-plot(f, main="",xlab="time",ylab="Proportion",pch=1+18*d$status)
+plot(f, main="",xlab="time",ylab="Proportion",pch=1+3*d$status)
 
 ## compare to Kaplan-Meier
 lines(survfit(Surv(time,status)~1,data=d),col="orange",conf.int=FALSE,lty=2,lwd=2)
